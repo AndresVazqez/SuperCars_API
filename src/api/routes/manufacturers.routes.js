0e8caba41c53096109d4630f3180ca1045cb13c8
AndresVazqez <aavsz0811@gmail.com>
2022-01-25 23:10:15 +0100

    Documentation

--- src/api/routes/manufacturers.routes.js
+++ src/api/routes/manufacturers.routes.js
@@ -5,7 +5,11 @@ const ManufacturersSchema = require('../models/manufacturers.model');
 
 router.route('/manufacturers').get(async (req, res) => {
     const manufacturers = await ManufacturersSchema.find();
-    res.status(200).json({manufacturers});
+    res.status(200).json({
+        status: 200,
+        message: HTTPSTATUSCODE[200],
+        data: { manufacturers: manufacturers },
+      });
 });
 
 //----------REPETIDO---------//
